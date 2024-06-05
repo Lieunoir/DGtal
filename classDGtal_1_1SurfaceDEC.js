d8749a278d66c3c65140b0aea9d112e72da35030
Deploy from CI <>
2024-06-05 18:23:17 +0200

    deploy new book

---
+++ classDGtal_1_1SurfaceDEC.js
@@ -0,0 +1,21 @@
+var classDGtal_1_1SurfaceDEC =
+[
+    [ "DenseMatrix", "classDGtal_1_1SurfaceDEC.html#a4f77f493599efdc33eb9be8680cbe7c2", null ],
+    [ "Index", "classDGtal_1_1SurfaceDEC.html#a423910facb0b092b898bebc157279d53", null ],
+    [ "LinearOperator", "classDGtal_1_1SurfaceDEC.html#a77d33a8b89bf43e3bdca86cf6a42ab6e", null ],
+    [ "D0", "classDGtal_1_1SurfaceDEC.html#af81dcb253ddfa6b8a72200837313924c", null ],
+    [ "Flat", "classDGtal_1_1SurfaceDEC.html#aefc97ca48bbc9881e2a85bcf21f87de5", null ],
+    [ "L0", "classDGtal_1_1SurfaceDEC.html#a5b161530e21d2d78146e618b1359e420", null ],
+    [ "localD0", "classDGtal_1_1SurfaceDEC.html#a03fdc4d9ca7d803fd534025c81312950", null ],
+    [ "localFlat", "classDGtal_1_1SurfaceDEC.html#abf62f62d7e2b368f7cccb84cc1715fdd", null ],
+    [ "localL0", "classDGtal_1_1SurfaceDEC.html#a04297cb67b61ce27f0349c8e6cbd5f89", null ],
+    [ "localM0", "classDGtal_1_1SurfaceDEC.html#ac1c75db968a0cdf8849d1757b3b069f7", null ],
+    [ "localM1", "classDGtal_1_1SurfaceDEC.html#aa8d001ae03ff746453f468c01c10da0d", null ],
+    [ "localM2", "classDGtal_1_1SurfaceDEC.html#a5f7a17ffa665259cf2e789646d7748d3", null ],
+    [ "localSharp", "classDGtal_1_1SurfaceDEC.html#a051005a1abf6e302b3177832fb0d2cce", null ],
+    [ "lumpedM0", "classDGtal_1_1SurfaceDEC.html#ad82b81db2e2ac758231f45ab251cc161", null ],
+    [ "M0", "classDGtal_1_1SurfaceDEC.html#a60b92ad9bbd10b2c827783da215fd599", null ],
+    [ "M1", "classDGtal_1_1SurfaceDEC.html#a915d41f97fc3a2b9766dfa3af11f438c", null ],
+    [ "M2", "classDGtal_1_1SurfaceDEC.html#a2f1b91be4f4feead9b571cf6117426d2", null ],
+    [ "Sharp", "classDGtal_1_1SurfaceDEC.html#aad9f5da9714e1908f54154f73e9982ff", null ]
+];
---
+++ search/pages_10.js
@@ -0,0 +1,13 @@
+var searchData=
+[
+  ['n_20dimensional_20dec_20structures_20into_20m_20dimensional_20space_0',['Embedding n-dimensional DEC structures into m-dimensional space',['../moduleDECEmbedding.html',1,'packageDEC']]],
+  ['nd_20fast_20marching_20methods_1',['nD Fast Marching Methods',['../moduleFMM.html',1,'packageGeometry']]],
+  ['nd_20volumetric_20analysis_20using_20separable_20processes_2',['nD Volumetric Analysis using Separable Processes',['../moduleVolumetric.html',1,'packageGeometry']]],
+  ['ndash_20beltrami_20operator_3',['Heat Kernel Laplace&amp;ndash;Beltrami Operator',['../moduleHeatLaplaceOperator.html',1,'packageDEC']]],
+  ['non_20manifold_20polygonal_20surfaces_20in_20r3_4',['Surface mesh data structure for representing manifold or non-manifold polygonal surfaces in R3',['../moduleSurfaceMesh.html',1,'packageShapes']]],
+  ['normal_20corrected_20finite_20element_20method_20for_20poisson_20problems_5',['Normal corrected Finite Element Method for Poisson problems',['../moduleNormalCorrectedFEM.html',1,'packageDEC']]],
+  ['normal_20estimator_6',['Maximal segments and slices of digital surface based normal estimator',['../moduleMaximalSegmentSliceEstimation.html',1,'packageGeometry']]],
+  ['normal_20estimators_7',['Plane-probing based normal estimators',['../modulePlaneProbing.html',1,'packageGeometry']]],
+  ['normal_20vector_20alignment_8',['Digital surface regularization by normal vector alignment',['../moduleRegularization.html',1,'packageGeometry']]],
+  ['number_20and_20integer_20concepts_9',['Number and Integer Concepts',['../moduleNumber.html',1,'packageKernel']]]
+];
